172e225c24fcfcbcbed57d901594eb045cb30485
Brandon Aaskov <brandonaaskov@gmail.com>
2016-02-09 09:24:36 -0800

    for the record, i fucking hate this bullshit
    After using `npm pack`, which has worked with varying degrees of success, I'm suddenly getting this strange error: https://github.com/facebook/react/issues/5538
    
    It sounds like it could _also_ be thrown if react is included twice, not that the error helps with that.
    
    Fuck this noise.

--- demos/demo.js
+++ demos/demo.js
@@ -18,10 +18,12 @@ class App extends Component {
   }
 
   onSelectionsChange (terms) {
+    console.log('onSelectionsChange', terms)
     this.setState({ terms })
   }
 
   onTypeaheadChange (tree, filterTerm) {
+    console.log('onTypeaheadChange', [filterTerm, tree])
     this.setState({ tree, filterTerm })
   }
 
