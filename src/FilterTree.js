5b8c7f4c9bd8d0b4c41b48b780f603d257900b82
Sadhana Jayanna <sjayanna@foundationmedicine.com>
2016-06-15 16:14:48 -0700

    replacing includes with isEqual

--- src/FilterTree.js
+++ src/FilterTree.js
@@ -54,7 +54,7 @@ export function getVisibleMatches (tree, text) {
 
   function contains (name, terms) {
     let filtered = _.filter(terms, (term) => {
-      return _.includes(name, _.lowerCase(term))
+      return _.isEqual(name, _.lowerCase(term))
     })
 
     return !_.isEmpty(filtered)
@@ -67,7 +67,7 @@ export function getVisibleMatches (tree, text) {
     if (_.isString(text)) {
       return name.indexOf(_.lowerCase(text)) > -1
     } else if (_.isArray(text)) {
-      return text.indexOf(node.name) > -1;
+      return contains(name, text)
     }
   })
 
